cdd2d584820153d6fb73dbcedd5cbac8febb024d
Peter Jones <pjones@devalot.com>
2017-06-22 14:45:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mynewt Tower
+
+A [Tower][] backend for [Apache Mynewt][].
+
+## Status
+
+Under active development.
+
+[apache mynewt]: http://mynewt.apache.org/
+[tower]: https://ivorylang.org/
---
+++ tower-mynewt/src/Ivory/OS/Mynewt/Tower/Mynewt.hs
@@ -0,0 +1,8 @@
+--------------------------------------------------------------------------------
+module Ivory.OS.Mynewt.Tower.Mynewt
+  ( compileTowerMynewt
+  ) where
+
+--------------------------------------------------------------------------------
+compileTowerMynewt :: IO ()
+compileTowerMynewt = undefined
